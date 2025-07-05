09bae7e3b7a64071e0ab3f1ee75d49d9ea4e3342
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-06 00:28:31 +0800

    Navbar: Add social links. Refactor mobile version

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,19 +2,36 @@ import homesvg from '../assets/home.svg'
 import aboutsvg from '../assets/about.svg'
 import skillsvg from '../assets/skill.svg'
 import projectssvg from '../assets/projects.svg'
+import githubsvg from '../assets/github.svg'
+import linkedinsvg from '../assets/linkedin.svg'
 
 const Navbar = () => {
     return (
         <div className="navbar">
             <div className="navbar-wrapper">
-                <span className="nav-item nav-text">Home</span>
-                <span className="nav-item nav-text">About</span>
-                <span className="nav-item nav-text">Skills</span>
-                <span className="nav-item nav-text">Project</span>
+                <div className="link-container">
+                    <span className="nav-item nav-text">Home</span>
+                    <span className="nav-item nav-text">About</span>
+                    <span className="nav-item nav-text">Skills</span>
+                    <span className="nav-item nav-text">Project</span>
+                </div>
+                
                 <img className="nav-item nav-icon" src={homesvg} alt="Home" />
                 <img className="nav-item nav-icon" src={aboutsvg} alt="About" />
                 <img className="nav-item nav-icon" src={skillsvg} alt="Skills" />
                 <img className="nav-item nav-icon" src={projectssvg} alt="Project" />
+
+                <div className="divider"></div>
+                
+                <div className="social-container">
+                    <a href="https://github.com/IkedaLab-Daniel">
+                        <img src={githubsvg} className='social' alt="github" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/mark-daniel-callejas-b18b37230/">
+                        <img src={linkedinsvg} className='social' alt="linkedin" />
+                    </a>
+                </div>
+                
             </div>
         </div>
     )
