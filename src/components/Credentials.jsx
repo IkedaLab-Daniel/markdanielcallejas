4d0c69d349ec6d7776b0925f8f063482d6cf871e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-07 06:59:32 +0800

    Add real badge icon. Carousel still bad

--- src/components/Credentials.jsx
+++ src/components/Credentials.jsx
@@ -1,6 +1,27 @@
-import { useEffect, useState } from 'react';
-import samplecert from '../assets/samplecert.jpeg'
-import samplebadge from '../assets/samplebadge.png'
+import { useState } from 'react';
+
+// > certificates
+import metafrontend from '../assets/metafrontend.jpeg'
+import metabackend from '../assets/metabackend.jpeg'
+import googleux from '../assets/googleux.jpeg'
+
+// > badges
+import badgefullstack from '../assets/badgefullstack.png'
+import badgefrontend from '../assets/badgefrontend.png'
+import badgebackend from '../assets/badgebackend.png'
+import ux from '../assets/badgeux.png'
+import js2 from '../assets/badgejs2.png'
+import js1 from '../assets/badgejs1.png'
+import cybersec from '../assets/badgecyber.png'
+import itess from '../assets/badgeitess.png'
+import badgehtml from '../assets/badgehtmless.png'
+import nodeexpress from '../assets/nodeexpress.png'
+import jsibm from '../assets/jsessibm.png'
+import frontendess from '../assets/frontendreact.png'
+import webdev from '../assets/webdev.png'
+import gitess from '../assets/gitess.png'
+import seess from '../assets/seess.png'
+import cloud from '../assets/cloud.png'
 import './credential.css';
 import { ExternalLink } from 'lucide-react';
 
@@ -8,58 +29,60 @@ const Credentials = () => {
   const featuredCertificates = [
     {
       id: 1,
-      title: "Certificate Certificate",
-      subtitle: "Coursera",
-      description: "Desription",
-      image: `${samplecert}`,
+      title: "Meta Front-End Developer Certificate",
+      subtitle: "Coursera | Meta",
+      description: "This 9-course program prepares learners for an entry-level career as a front-end developer.",
+      image: `${metafrontend}`,
       verificationUrl: "https://credly.com/badges/example-1",
-      technologies: ["React", "Django", "MySQL"]
+      technologies: ["HTML", "CSS", "JavaScript", "React", "Responsive Design", "Git", "GitHub"]
     },
     {
       id: 2,
-      title: "Certificate Certificate",
-      subtitle: "Coursera",
-      description: "Desription Desription Desription",
-      image: `${samplecert}`,
+      title: "Meta Back-End Developer Certificate",
+      subtitle: "Coursera | Meta",
+      description: "This 9-course program prepares learners for an entry-level career as a oack-end developer.",
+      image: `${metabackend}`,
       verificationUrl: "https://credly.com/badges/example-2",
-      technologies: ["React", "Django", "MySQL"]
+      technologies: ["Python", "Django", "REST-API", "MySQL", "Web Security"]
     },
     {
       id: 3,
-      title: "Certificate Certificate",
-      subtitle: "Coursera",
-      description: "Desription Desription Desription",
-      image: `${samplecert}`,
+      title: "Google UX Design",
+      subtitle: "Coursera | Google",
+      description: "Include hands-on, practice-based assessments and are designed to prepare them for introductory-level roles in UX design.",
+      image: `${googleux}`,
       verificationUrl: "https://credly.com/badges/example-3",
-      technologies: ["React", "Django", "MySQL"]
+      technologies: ["UX Design", "User Research","Prototyping", "Figma", "Responsive & Accessibility Design"]
     }
   ];
 
   const allBadges = [
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
-    { id: 1, name: "badge1", icon: `${samplebadge}` },
+    { id: 1, name: "badge1", icon: `${badgefullstack}` },
+    { id: 1, name: "badge1", icon: `${badgefrontend}` },
+    { id: 1, name: "badge1", icon: `${badgebackend}` },
+    { id: 1, name: "badge1", icon: `${ux}` },
+    { id: 1, name: "badge1", icon: `${js2}` },
+    { id: 1, name: "badge1", icon: `${js1}` },
+    { id: 1, name: "badge1", icon: `${cybersec}` },
+    { id: 1, name: "badge1", icon: `${itess}` },
+    { id: 1, name: "badge1", icon: `${badgehtml}` },
+    { id: 1, name: "badge1", icon: `${nodeexpress}` },
+    { id: 1, name: "badge1", icon: `${jsibm}` },
+    { id: 1, name: "badge1", icon: `${frontendess}` },
+    { id: 1, name: "badge1", icon: `${webdev}` },
+    { id: 1, name: "badge1", icon: `${gitess}` },
+    { id: 1, name: "badge1", icon: `${seess}` },
+    { id: 1, name: "badge1", icon: `${cloud}` },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex(prev => (prev + 1) % allBadges.length);
-    }, 2000);
-    return () => clearInterval(interval);
-  }, [allBadges.length]);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentIndex(prev => (prev + 1) % allBadges.length);
+  //   }, 2000);
+  //   return () => clearInterval(interval);
+  // }, [allBadges.length]);
 
   return (
     <section className="credentials-section">
@@ -95,22 +118,13 @@ const Credentials = () => {
           </div>
 
           <div className="skills-section">
-            {/* <h3>Technical Skills</h3>
-            <p>Technologies and frameworks I work with</p> */}
             <div className="carousel">
-              <div
-                className="carousel-track"
-                style={{
-                  transform: `translateX(-${(currentIndex * 100) / 6}%)`,
-                  width: `${(allBadges.length * 300) / 6}px`,
-                }}
-              >
-                {[...allBadges, ...allBadges, ...allBadges].map((badge, index) => (
+              <div className="carousel-track">
+                {[...allBadges, ...allBadges].map((badge, index) => (
                   <div className="badge" key={`${badge.id}-${index}`}>
                     <div className="icon">
-                        <img src={badge.icon} alt="" />
+                      <img src={badge.icon} alt={badge.name} />
                     </div>
-                    {/* <span>{badge.name}</span> */}
                   </div>
                 ))}
               </div>
