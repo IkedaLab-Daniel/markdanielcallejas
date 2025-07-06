458f9b885faa33eb318ce7235623f4125e1fed0e
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-06 21:51:35 +0800

    Done AOS

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -28,7 +28,7 @@ const About = () => {
                 <div 
                   className="stat-card" 
                   key={index}
-                  data-aos="fade-up"
+                  data-aos="flip-down"
                   data-aos-delay={index * 100}
                 >
                     <div className="stat-icon">{stat.icon}</div>
@@ -41,7 +41,7 @@ const About = () => {
             ))}
         </div>
 
-        <div className="about-right">
+        <div className="about-right" data-aos="fade-up">
             <h2 className="about-heading">About</h2>
             <p className="about-quote"><em>“Spend nights improving...”</em></p>
             <p className="about-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus praesentium, beatae sed ullam ipsam distinctio consequuntur quos autem dolor accusantium corporis quibusdam alias eius sunt est fugit tenetur ab laboriosam.</p>
