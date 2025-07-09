911c58a68a28c8f0d245e922af64750ddd7f6aea
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-09 22:37:39 +0800

    Add imgs and data for projects

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,7 +2,9 @@ import "./ProjectsPage.css";
 
 // > images - mockups
 import explainora from '../assets/explainora-square.png'
-
+import freedomnotes from '../assets/freedomnotes-square.png'
+import phpets from '../assets/phpets-square.png'
+import cvdrive from '../assets/cvdrive-square.png'
 const projects = [
     {
         id: 1,
@@ -10,8 +12,8 @@ const projects = [
         subtitle: "Analyze, debug, improve code using AI",
         description:
             "Explainora.ai is a full-stack AI-powered web app that helps users understand and get explanations for code snippets. It allows users to input code and receive intelligent explanations using integrated AI models. Ideal for developers, students, and anyone learning to code.",
-        tags: ["React", "React"],
-        features: ["Feature", "Feature", "Feature"],
+        tags: ["React", "CSS", "Node", "Express"],
+        features: ["Fast Explain", "Smart Explain", "Suggestions"],
         stack: ["react", "css", "node", "express"],
         image: `${explainora}`,
         codeLink: "#",
@@ -19,14 +21,40 @@ const projects = [
     },
     {
         id: 2,
-        title: "Title Title Title Title",
-        subtitle: "Subtitle Subtitle Subtitle Subtitle Subtitl",
+        title: "FreedomNotes",
+        subtitle: "MERN Stack Freedom-Wall-Like Web Application",
         description:
-            "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... Neque porro quisquam est...",
-        tags: ["React", "React"],
+            "Freedom Notes is a web application for creating, sharing, and managing notes within a board-based system. It provides features for both public and anonymous note posting, note approval by admins, and user authentication.",
+        tags: ["React", "CSS", "Node", "Express", "MongoDB"],
         features: ["Feature", "Feature", "Feature"],
         stack: ["react", "css", "node", "express"],
-        image: `${explainora}`,
+        image: `${freedomnotes}`,
+        codeLink: "#",
+        demoLink: "#",
+    },
+    {
+        id: 3,
+        title: "PHPets",
+        subtitle: "E-commerce Website built with PHP and MySQL",
+        description:
+            "A simple e-commerce platform built in PHP for managing pet products, shopping cart, and checkout workflows.",
+        tags: ["PHP", "CSS", "MySQL"],
+        features: ["Feature", "Feature", "Feature"],
+        stack: ["react", "css", "node", "express"],
+        image: `${phpets}`,
+        codeLink: "#",
+        demoLink: "#",
+    },
+    {
+        id: 4,
+        title: "CV Drive",
+        subtitle: "File Storing Stystem",
+        description:
+            "My first full stack web application.",
+        tags: ["React", "CSS", "Node", "Express", "MongoDB"],
+        features: ["Feature", "Feature", "Feature"],
+        stack: ["react", "css", "node", "express"],
+        image: `${cvdrive}`,
         codeLink: "#",
         demoLink: "#",
     },
